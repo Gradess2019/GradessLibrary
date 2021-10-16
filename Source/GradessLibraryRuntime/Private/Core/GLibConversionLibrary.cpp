75fe9528544e51b0d81ede486000a192d8d180b5
Gradess <trofimovjava@gmail.com>
2021-10-17 00:53:54 +0300

    Add conversions

---
+++ Source/GradessLibraryRuntime/Private/Core/GLibConversionLibrary.cpp
@@ -0,0 +1,134 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "GradessLibraryRuntime/Public/Core/GLibConversionLibrary.h"
+#include "UObject/UnrealType.h"
+#include "Templates/SubclassOf.h"
+
+FString UGLibConversionLibrary::ConvertToString(const int64& Integer)
+{
+	return FString::Printf(TEXT("%lld"), Integer);
+}
+
+TArray<FString> UGLibConversionLibrary::ConvertNameArrayToStringArray(TArray<FName>& Names)
+{
+	TArray<FString> Strings;
+	ConvertArray<FName, FString>(Names, Strings, [](const FName& Name) { return Name.ToString(); });
+	
+	return Strings;
+}
+
+TArray<FText> UGLibConversionLibrary::ConvertNameArrayToTextArray(const TArray<FName>& Names)
+{
+	TArray<FText> Texts;
+	ConvertArray<FName, FText>(Names, Texts, [](const FName& name) { return FText::FromName(name); });
+
+	return Texts;
+}
+
+TArray<FName> UGLibConversionLibrary::ConvertStringArrayToNameArray(const TArray<FString>& Strings)
+{
+	TArray<FName> Names;
+	ConvertArray<FString, FName>(Strings, Names, [](const FString& String) { return FName(String); });
+
+	return Names;
+}
+
+TArray<FText> UGLibConversionLibrary::ConvertStringArrayToTextArray(const TArray<FString>& Strings)
+{
+	TArray<FText> Texts;
+	ConvertArray<FString, FText>(Strings, Texts, [](const FString& String) { return FText::FromString(String); });
+
+	return Texts;
+}
+
+TArray<int32> UGLibConversionLibrary::ConvertStringArrayToIntArray(const TArray<FString>& Strings)
+{
+	TArray<int32> Integers;
+	ConvertArray<FString, int32>(Strings, Integers, [](const FString& String) { return FCString::Atoi(*String); });
+
+	return Integers;
+}
+
+TArray<int64> UGLibConversionLibrary::ConvertStringArrayToInt64Array(const TArray<FString>& Strings)
+{
+	TArray<int64> Integers;
+	ConvertArray<FString, int64>(Strings, Integers, [](const FString& String) { return FCString::Atoi64(*String); });
+
+	return Integers;
+}
+
+TArray<float> UGLibConversionLibrary::ConvertStringArrayToFloatArray(const TArray<FString>& Strings)
+{
+	TArray<float> Floats;
+	ConvertArray<FString, float>(Strings, Floats, [](const FString& String) { return FCString::Atof(*String); });
+
+	return Floats;
+}
+
+TArray<FName> UGLibConversionLibrary::ConvertTextArrayToNameArray(const TArray<FText>& Texts)
+{
+	TArray<FName> names;
+	ConvertArray<FText, FName>(Texts, names, [](const FText& text) { return FName(text.ToString()); });
+
+	return names;
+}
+
+TArray<FString> UGLibConversionLibrary::ConvertTextArrayToStringArray(const TArray<FText>& Texts)
+{
+	TArray<FString> Strings;
+	ConvertArray<FText, FString>(Texts, Strings, [](const FText& text) { return text.ToString(); });
+
+	return Strings;
+}
+
+TArray<UObject*> UGLibConversionLibrary::ConvertObjectArray_Internal(
+	const TArray<UObject*>& Objects,
+	TSubclassOf<UObject> ObjectClass,
+	const bool bExcludeNullptr
+)
+{
+	check(0);
+	return TArray<UObject*>();
+}
+
+template <typename From, typename To>
+void UGLibConversionLibrary::ConvertArray(
+	const TArray<From>& SourceArray,
+	TArray<To>& ConvertedArray,
+	std::function<To(const From&)> ConversionFunction
+)
+{
+	ConvertedArray.Reset();
+	ConvertedArray.Reserve(SourceArray.Num());
+	for (auto& element : SourceArray) { ConvertedArray.Add(ConversionFunction(element)); }
+}
+
+DEFINE_FUNCTION(UGLibConversionLibrary::execConvertObjectArray_Internal)
+{
+	Stack.MostRecentPropertyAddress = NULL;
+	Stack.MostRecentProperty = NULL;
+
+	P_GET_TARRAY_REF(UObject*, Objects);
+	P_GET_OBJECT(UClass, ObjectClass);
+	P_GET_UBOOL8(bExcludeNullptr)
+	
+	P_FINISH;
+	
+	P_NATIVE_BEGIN;
+
+	TArray<UObject*> OutArray;
+	OutArray.Reserve(Objects.Num());
+	
+	for (const auto Element : Objects)
+	{
+		const auto ElementToAdd = Element && Element->IsA(ObjectClass) ? Element : nullptr;
+	
+		if (bExcludeNullptr && !ElementToAdd) { continue; }
+		OutArray.Add(ElementToAdd);
+	}
+	
+	*(TArray<UObject*>*)RESULT_PARAM = OutArray;
+	
+	P_NATIVE_END;
+}
---
+++ Source/GradessLibraryRuntime/Public/Core/GLibConversionLibrary.h
@@ -0,0 +1,231 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include <functional>
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GLibConversionLibrary.generated.h"
+
+/**
+ * Library that is used for converting arrays to different array type, int64 to string, etc.
+ */
+UCLASS()
+class GRADESSLIBRARYRUNTIME_API UGLibConversionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * @brief Converts integer to string
+	 * @param Integer integer to convert
+	 * @return converted integer to string
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToString (int64)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static FString ConvertToString(const int64& Integer);
+
+	/**
+	* @brief Converts array of names to array of strings
+	* @param Names array of names
+	* @return converted array 
+	* @return converted array 
+	*/
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToString (name array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<FString> ConvertNameArrayToStringArray(
+		UPARAM(ref) TArray<FName>& Names
+	);
+
+	/**
+	* @brief Converts array of names to array of texts
+	* @param Names array of names
+	* @return converted array 
+	*/
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToText (name array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<FText> ConvertNameArrayToTextArray(
+		const TArray<FName>& Names
+	);
+
+	/**
+	* @brief Converts array of strings to array of names
+	* @param Strings array of strings
+	* @return converted array 
+	*/
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToName (string array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<FName> ConvertStringArrayToNameArray(
+		UPARAM(ref) const TArray<FString>& Strings
+	);
+
+	/**
+	 * @brief Converts array of strings to array of texts
+	 * @param Strings array of strings
+	 * @return converted array 
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToText (string array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<FText> ConvertStringArrayToTextArray(
+		UPARAM(ref) const TArray<FString>& Strings
+	);
+
+	/**
+	 * @brief Converts array of strings to array of integers
+	 * @param Strings array of strings
+	 * @return converted array 
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToInt (string array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<int32> ConvertStringArrayToIntArray(
+		UPARAM(ref) const TArray<FString>& Strings
+	);
+
+	/**
+	 * @brief Converts array of strings to array of 64-bit integers
+	 * @param Strings array of strings
+	 * @return converted array 
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToInt64 (string array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<int64> ConvertStringArrayToInt64Array(
+		UPARAM(ref) const TArray<FString>& Strings
+	);
+
+	/**
+	 * @brief Converts array of strings to array of floats
+	 * @param Strings array of strings
+	 * @return converted array 
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToFloat (string array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<float> ConvertStringArrayToFloatArray(
+		UPARAM(ref) const TArray<FString>& Strings
+	);
+
+	/**
+	 * @brief Converts array of texts to array of strings
+	 * @param Texts array of texts
+	 * @return converted array 
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToName (text array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<FName> ConvertTextArrayToNameArray(
+		UPARAM(ref) const TArray<FText>& Texts
+	);
+
+	/**
+	 * @brief Converts array of texts to array of strings
+	 * @param Texts array of texts
+	 * @return converted array 
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "ToString (text array)",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static TArray<FString> ConvertTextArrayToStringArray(
+		UPARAM(ref) const TArray<FText>& Texts
+	);
+
+	/**
+	* @brief Cast array of objects from one type to another
+	* @param Objects object array to cast
+	* @param ObjectClass to which class cast
+	* @param bExcludeNullptr if true, exclude all unsuccessful casted object, false otherwise
+	* @return casted array 
+	*/
+	UFUNCTION(
+		BlueprintPure,
+		CustomThunk,
+		Category="GLib|Conversions",
+		meta = (
+			DisplayName = "Convert Object Array",
+			DeterminesOutputType = "ObjectClass",
+			CustomStructureParam = "Objects",
+			BlueprintInternalUseOnly
+		)
+	)
+	static TArray<UObject*> ConvertObjectArray_Internal(
+		const TArray<UObject*>& Objects,
+		TSubclassOf<UObject> ObjectClass,
+		const bool bExcludeNullptr
+	);
+
+	template <typename From, typename To>
+	static void ConvertArray(
+		const TArray<From>& SourceArray,
+		TArray<To>& ConvertedArray,
+		std::function<To(const From&)> ConversionFunction
+	);
+
+private:
+	DECLARE_FUNCTION(execConvertObjectArray_Internal);
+};

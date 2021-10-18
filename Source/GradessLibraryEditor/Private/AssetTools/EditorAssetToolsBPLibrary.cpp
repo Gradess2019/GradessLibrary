de1afba16920b215e661a2c5ec293ee119572516
Gradess <trofimovjava@gmail.com>
2021-10-18 21:43:59 +0300

    Add editor asset tools library

---
+++ Source/GradessLibraryEditor/Private/AssetTools/EditorAssetToolsBPLibrary.cpp
@@ -0,0 +1,141 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "AssetTools/EditorAssetToolsBPLibrary.h"
+#include "PackageTools.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "Misc/AssetRegistryInterface.h"
+
+
+UPackage* UEditorAssetToolsBPLibrary::GetPackage(const FName& PackagePath)
+{
+	return FindPackage(nullptr, *PackagePath.ToString());
+}
+
+bool UEditorAssetToolsBPLibrary::UnloadPackage(const FName& PackagePath)
+{
+	const auto Package = FindPackage(nullptr, *PackagePath.ToString());
+	return UPackageTools::UnloadPackages({Package});
+}
+
+bool UEditorAssetToolsBPLibrary::UnloadPackageAndDependencies(const FName& PackagePath)
+{
+	if (!UnloadPackage(PackagePath))
+	{
+		return false;
+	}
+
+	auto bSuccess = true;
+
+	TArray<FName> Dependencies;
+	GetHardDependencies(PackagePath, Dependencies);
+
+	TSet<FName> PackagesToUnload;
+	GetUnreferencedPackages(Dependencies, PackagesToUnload);
+
+	// Unload till we have loaded dependencies
+	while (PackagesToUnload.Num() > 0)
+	{
+		TSet<FName> ChildDependencies;
+		bSuccess = bSuccess && UnloadDependencies(PackagesToUnload, ChildDependencies);
+
+		PackagesToUnload = ChildDependencies;
+	}
+
+	return bSuccess;
+}
+
+bool UEditorAssetToolsBPLibrary::UnloadDependencies(
+	TSet<FName>& Dependencies,
+	TSet<FName>& OutChildDependencies
+)
+{
+	TArray<UPackage*> Packages;
+	for (auto& Dependency : Dependencies)
+	{
+		const auto Package = FindPackage(nullptr, *Dependency.ToString());
+		Packages.Add(Package);
+	}
+
+	const auto bSuccess = UPackageTools::UnloadPackages(Packages);
+	if (!bSuccess)
+	{
+		return false;
+	}
+
+	for (auto& Dependency : Dependencies)
+	{
+		TArray<FName> ChildDependencies;
+		GetHardDependencies(Dependency, ChildDependencies);
+
+		TSet<FName> UnreferencedChildDependencies;
+		GetUnreferencedPackages(ChildDependencies, UnreferencedChildDependencies);
+
+		OutChildDependencies.Append(UnreferencedChildDependencies);
+	}
+
+	return true;
+}
+
+void UEditorAssetToolsBPLibrary::GetUnreferencedPackages(const TArray<FName>& PackagePaths, TSet<FName>& OutUnreferencedPackagePaths)
+{
+	for (auto& Package : PackagePaths)
+	{
+		if (!IsProjectPackage(Package) || IsAnybodyReferenced(Package))
+		{
+			continue;
+		}
+
+		GLog->Logf(TEXT("Unreferenced package: %s"), *Package.ToString());
+		OutUnreferencedPackagePaths.Add(Package);
+	}
+}
+
+void UEditorAssetToolsBPLibrary::GetHardDependencies(const FName& PackagePath, TArray<FName>& OutDependencies)
+{
+	const auto& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+	AssetRegistry.Get().GetDependencies(
+		PackagePath,
+		OutDependencies,
+		UE::AssetRegistry::EDependencyCategory::Package,
+		UE::AssetRegistry::FDependencyQuery(
+			UE::AssetRegistry::EDependencyQuery::Hard
+		)
+	);
+}
+
+void UEditorAssetToolsBPLibrary::GetHardReferencers(const FName& PackagePath, TArray<FName>& OutReferencers)
+{
+	const auto& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry").Get();
+	AssetRegistry.GetReferencers(
+		PackagePath,
+		OutReferencers,
+		UE::AssetRegistry::EDependencyCategory::Package,
+		UE::AssetRegistry::FDependencyQuery(
+			UE::AssetRegistry::EDependencyQuery::Hard
+		)
+	);
+}
+
+bool UEditorAssetToolsBPLibrary::IsProjectPackage(const FName& Package)
+{
+	return Package.ToString().StartsWith("/Game");
+}
+
+bool UEditorAssetToolsBPLibrary::IsAnybodyReferenced(const FName& PackagePath)
+{
+	const auto& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry").Get();
+
+	TArray<FName> Referencers;
+	GetHardReferencers(PackagePath, Referencers);
+
+	for (const auto& Referencer : Referencers)
+	{
+		auto AssetData = AssetRegistry.GetAssetByObjectPath(Referencer);
+		if (AssetData.IsAssetLoaded())
+		{
+			return true;
+		}
+	}
+	return false;
+}
---
+++ Source/GradessLibraryEditor/Public/AssetTools/EditorAssetToolsBPLibrary.h
@@ -0,0 +1,133 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorAssetToolsBPLibrary.generated.h"
+
+/**
+ * @brief A function library that provides additional useful functions such as unloading assets in editor
+ */
+UCLASS()
+class GRADESSLIBRARYEDITOR_API UEditorAssetToolsBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * @brief Returns UPackage object by path
+	 * @param PackagePath path to package
+	 * @return package object
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		BlueprintCallable,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static UPackage* GetPackage(const FName& PackagePath);
+
+	/**
+	 * @brief Unloads package by path
+	 * @param PackagePath package to unload
+	 * @return true if successful, false otherwise
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static bool UnloadPackage(const FName& PackagePath);
+
+	/**
+	 * @brief Unloads a package and its descendants, excluding assets that is currently referenced by other loaded assets
+	 * @param PackagePath package to unload
+ 	 * @return true if successful, false otherwise
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static bool UnloadPackageAndDependencies(const FName& PackagePath);
+
+	/**
+	 * @brief Unloads dependencies
+	 * @param Dependencies dependencies to unload
+	 * @param OutChildDependencies dependencies of passed dependencies
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static bool UnloadDependencies(
+		UPARAM(ref) TSet<FName>& Dependencies,
+		UPARAM(DisplayName="ChildDependencies") TSet<FName>& OutChildDependencies
+	);
+
+	/**
+	 * @brief Returns set of unreferenced packages
+	 * @param PackagePaths array of packages to check
+	 * @param OutUnreferencedPackagePaths set of unreferenced, but loaded packages
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static void GetUnreferencedPackages(
+		const TArray<FName>& PackagePaths,
+		UPARAM(DisplayName="UnreferencedPackages") TSet<FName>& OutUnreferencedPackagePaths
+	);
+
+	/**
+	 * @brief Returns package hard dependencies 
+	 * @param PackagePath package to obtain hard dependencies
+	 * @param OutDependencies obtained dependencies
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static void GetHardDependencies(
+		const FName& PackagePath,
+		UPARAM(DisplayName="Dependencies") TArray<FName>& OutDependencies
+	);
+
+	/**
+	* @brief Returns package hard referencers 
+	* @param PackagePath package to obtain hard referencers
+	* @param OutReferencers obtained referencers
+	*/
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static void GetHardReferencers(
+		const FName& PackagePath,
+		UPARAM(DisplayName="Referencers") TArray<FName>& OutReferencers
+	);
+
+	/**
+	 * @brief Checks if given package is a project package
+	 * @param Package package to check
+	 * @return true if package is in "/Game" directory, false otherwise
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static bool IsProjectPackage(const FName& Package);
+
+	/**
+	 * @brief Checks if anybody refers to the given package
+	 * @param PackagePath package to check
+	 * @return true if someone refers, false otherwise
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "GLib|EditorAssetToolsBPLibrary"
+	)
+	static bool IsAnybodyReferenced(const FName& PackagePath);
+};

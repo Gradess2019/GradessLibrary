4104e5d8c10901a58a0774fbe102db6324938ef3
Gradess <trofimovjava@gmail.com>
2021-10-20 00:04:02 +0300

    Connect python widget type actions to module startup

--- Source/GradessLibraryEditor/Public/GradessLibraryEditor.h
+++ Source/GradessLibraryEditor/Public/GradessLibraryEditor.h
@@ -5,10 +5,19 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
+class FAssetTypeActions_GLibPythonEditorWidgetBlueprint;
+
 class FGradessLibraryEditorModule : public IModuleInterface
 {
 public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+private:
+	TSharedPtr<FAssetTypeActions_GLibPythonEditorWidgetBlueprint> Actions;
+
+	void RegisterPythonEditorWidgetActions();
+
+	void UnregisterPythonEditorWidgetActions();
 };

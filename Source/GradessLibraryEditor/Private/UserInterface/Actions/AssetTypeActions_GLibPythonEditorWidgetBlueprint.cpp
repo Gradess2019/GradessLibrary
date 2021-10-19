1982d979f8243b1617461d5ec1db5403ad02ca5c
Gradess <trofimovjava@gmail.com>
2021-10-19 23:54:55 +0300

    Add python editor widget asset type actions

---
+++ Source/GradessLibraryEditor/Private/UserInterface/Actions/AssetTypeActions_GLibPythonEditorWidgetBlueprint.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "UserInterface/Actions/AssetTypeActions_GLibPythonEditorWidgetBlueprint.h"
+#include "WidgetBlueprintEditor.h"
+#include "UserInterface/Assets/GLibPythonEditorWidgetBlueprint.h"
+
+#define LOCTEXT_NAMESPACE "GLibAssetTypeActions"
+
+FText FAssetTypeActions_GLibPythonEditorWidgetBlueprint::GetName() const
+{
+	return LOCTEXT("AssetTypeActions_GLibPythonEditorWidget", "Python Editor Widget");
+}
+
+UClass* FAssetTypeActions_GLibPythonEditorWidgetBlueprint::GetSupportedClass() const
+{
+	return UGLibPythonEditorWidgetBlueprint::StaticClass();
+}
+
+FColor FAssetTypeActions_GLibPythonEditorWidgetBlueprint::GetTypeColor() const
+{
+	return FColor(0, 169, 255);
+}
+
+uint32 FAssetTypeActions_GLibPythonEditorWidgetBlueprint::GetCategories()
+{
+	if (!FModuleManager::Get().IsModuleLoaded("AssetTools")) { return EAssetTypeCategories::Misc; }
+
+	const auto& AssetTools = FModuleManager::GetModuleChecked<FAssetToolsModule>("AssetTools").Get();
+	return AssetTools.FindAdvancedAssetCategory("EditorUtilities");;
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/GradessLibraryEditor/Public/UserInterface/Actions/AssetTypeActions_GLibPythonEditorWidgetBlueprint.h
@@ -0,0 +1,17 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetTypeActions_Base.h"
+
+/**
+ * 
+ */
+class GRADESSLIBRARYEDITOR_API FAssetTypeActions_GLibPythonEditorWidgetBlueprint : public FAssetTypeActions_Base
+{
+	virtual FText GetName() const override;
+	virtual UClass* GetSupportedClass() const override;
+	virtual FColor GetTypeColor() const override;
+	virtual uint32 GetCategories() override;
+};

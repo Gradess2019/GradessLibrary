e2b50a49c7e66819dac53664a74b974fb527f0cb
Gradess <trofimovjava@gmail.com>
2021-10-23 17:28:03 +0300

    Add GAS base character

---
+++ Source/GradessGASLibrary/Private/Characters/GLibGASCharacter.cpp
@@ -0,0 +1,73 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Characters/GLibGASCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+AGLibGASCharacter::AGLibGASCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+	SetReplicatingMovement(true);
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Full);
+}
+
+void AGLibGASCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
+
+void AGLibGASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AbilitySystemComponent);
+
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
+
+UAbilitySystemComponent* AGLibGASCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+bool AGLibGASCharacter::GrantAbility(
+	const TSubclassOf<UGameplayAbility> AbilityClass,
+	int32 Level,
+	int32 InputCode,
+	FGameplayAbilitySpecHandle& OutAbilitySpecHandle
+)
+{
+	check(AbilitySystemComponent);
+	if (!HasAuthority() || !IsValid(AbilityClass)) { return false; }
+
+	const auto NewAbility = AbilityClass.GetDefaultObject();
+	if (!IsValid(NewAbility)) { return false; }
+
+	const auto AbilitySpec = FGameplayAbilitySpec(AbilityClass, Level, InputCode);
+
+	OutAbilitySpecHandle = AbilitySystemComponent->GiveAbility(AbilitySpec);
+	return true;
+}
+
+void AGLibGASCharacter::ActivateAbility(const int32 InputCode)
+{
+	check(AbilitySystemComponent);
+	AbilitySystemComponent->AbilityLocalInputPressed(InputCode);
+}
+
+void AGLibGASCharacter::CancelAbilityWithTags(
+	const FGameplayTagContainer& WithTags,
+	const FGameplayTagContainer& WithoutTags,
+	UGameplayAbility* Ignore
+)
+{
+	check(AbilitySystemComponent);
+	AbilitySystemComponent->CancelAbilities(&WithTags, &WithoutTags, Ignore);
+}
---
+++ Source/GradessGASLibrary/Public/Characters/GLibGASCharacter.h
@@ -0,0 +1,96 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayTagContainer.h"
+#include "GameplayAbilitySpec.h"
+#include "GameFramework/Character.h"
+#include "GLibGASCharacter.generated.h"
+
+struct FGameplayTagContainer;
+class UAbilitySystemComponent;
+class UGameplayAbility;
+
+/**
+ * @brief Base GAS character
+ */
+UCLASS(
+	BlueprintType,
+	Blueprintable,
+	ClassGroup = (
+		GLib
+	)
+)
+class GRADESSGASLIBRARY_API AGLibGASCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AGLibGASCharacter();
+
+	/**
+	 * @brief Ability System Component. Required to use Gameplay Attributes and Gameplay Abilities.
+	 */
+	UPROPERTY(
+		VisibleDefaultsOnly,
+		BlueprintReadOnly,
+		Category = "Abilities"
+	)
+	UAbilitySystemComponent* AbilitySystemComponent;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void BeginPlay() override;
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	/**
+	 * @brief Grants an ability at the given level, with an input code used to pick and choose which ability should be triggered.
+	 * @param AbilityClass Ability to create
+	 * @param Level level of ability
+	 * @param InputCode input code to trigger ability
+	 * @param OutAbilitySpecHandle created ability spec handle
+	 * @return true if success, false otherwise
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "GLib|GASCharacter"
+	)
+	bool GrantAbility(
+		const TSubclassOf<UGameplayAbility> AbilityClass,
+		int32 Level,
+		int32 InputCode,
+		UPARAM(DisplayName = "AbilitySpecHandle") FGameplayAbilitySpecHandle& OutAbilitySpecHandle
+	);
+
+	/**
+	 * @brief Activates an ability with a matching input code
+	 * @param InputCode input code to trigger ability
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "GLib|GASCharacter"
+	)
+	void ActivateAbility(const int32 InputCode);
+
+	/**
+	 * @brief Cancel all abilities with the specified tags. Will not cancel the Ignore instance
+	 * @param WithTags cancel abilities with tags
+	 * @param WithoutTags cancel abilities without tags
+	 * @param Ignore ability to ignore
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "GLib|GASCharacter",
+		meta = (
+			AutoCreateRefTerm = "WithTags, WithoutTags"
+		)
+	)
+	void CancelAbilityWithTags(
+		const FGameplayTagContainer& WithTags,
+		const FGameplayTagContainer& WithoutTags,
+		UGameplayAbility* Ignore = nullptr
+	);
+};

f032185665eb3d11320e6b8822a42d290d9a2e03
Gradess <trofimovjava@gmail.com>
2021-10-24 23:14:10 +0300

    Add player controller with delegates

---
+++ Source/GradessLibraryRuntime/Private/Controllers/GLibPlayerController.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Controllers/GLibPlayerController.h"
+
+#include "GameFramework/PlayerInput.h"
+
+bool AGLibPlayerController::InputKey(FKey Key, EInputEvent Event, float AmountDepressed, bool bGamepad)
+{
+	const auto Result = Super::InputKey(Key, Event, AmountDepressed, bGamepad);
+	if (Event == IE_Pressed)
+	{
+		OnKeyPressed.Broadcast(Key);
+	}
+	else if (Event == IE_Released)
+	{
+		OnKeyReleased.Broadcast(Key);
+	}
+	
+	return Result;
+}

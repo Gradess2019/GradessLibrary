0b8b0379bd8fec66931d35e64cb9e58e210b9640
Gradess <trofimovjava@gmail.com>
2021-10-26 02:27:56 +0300

    Add gameplay effect library

---
+++ Source/GradessGASLibrary/Private/Libraries/GLibGameplayEffectLibrary.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Libraries/GLibGameplayEffectLibrary.h"
+
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+
+int32 UGLibGameplayEffectLibrary::GetGameplayEffectLevelByHandle(const FActiveGameplayEffectHandle& Handle)
+{
+	const auto AbilitySystem = Handle.GetOwningAbilitySystemComponent();
+	check(AbilitySystem)
+
+	const auto Effect = AbilitySystem->GetActiveGameplayEffect(Handle);
+	return Effect->Spec.GetLevel();
+}
+
+int32 UGLibGameplayEffectLibrary::GetGameplayEffectLevelBySpec(const FGameplayEffectSpec& Spec)
+{
+	return Spec.GetLevel();
+}
+
+void UGLibGameplayEffectLibrary::AddGameplayEffectLevelByHandle(
+	FActiveGameplayEffectHandle& Handle,
+	const int32 Delta
+)
+{
+	const auto AbilitySystem = Handle.GetOwningAbilitySystemComponent();
+	const auto CurrentLevel = GetGameplayEffectLevelByHandle(Handle);
+	GLog->Logf(TEXT("%d"), Delta);
+	AbilitySystem->SetActiveGameplayEffectLevel(Handle, CurrentLevel + Delta);
+}
+
+void UGLibGameplayEffectLibrary::AddGameplayEffectLevelByTags(
+	UAbilitySystemComponent*& AbilitySystem,
+	const FGameplayTagContainer& Tags,
+	const int32 Delta
+)
+{
+	auto Effects = AbilitySystem->GetActiveEffectsWithAllTags(Tags);
+	for (auto& Effect : Effects)
+	{
+		AddGameplayEffectLevelByHandle(Effect, Delta);
+	}
+}
---
+++ Source/GradessGASLibrary/Public/Libraries/GLibGameplayEffectLibrary.h
@@ -0,0 +1,76 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "GLibGameplayEffectLibrary.generated.h"
+
+/**
+ * @brief Library that is providing function for working with gameplay effects
+ */
+UCLASS()
+class GRADESSGASLIBRARY_API UGLibGameplayEffectLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * @brief Returns current effect level by effect handle
+	 * @param Handle effect handle to use
+	 * @return current effect level
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category = "GLib|GameplayEffectLibrary"
+	)
+	static int32 GetGameplayEffectLevelByHandle(
+		const FActiveGameplayEffectHandle& Handle
+	);
+
+	/**
+	 * @brief Returns current effect level by effect spec
+	 * @param Spec effect spec to use
+	 * @return current effect level
+	 */
+	UFUNCTION(
+		BlueprintPure,
+		Category = "GLib|GameplayEffectLibrary"
+	)
+	static int32 GetGameplayEffectLevelBySpec(const FGameplayEffectSpec& Spec);
+
+	/**
+	 * @brief Increases gameplay effect level by delta
+	 * @param Handle effect handle to use
+	 * @param Delta delta level, usually it is 1
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintAuthorityOnly,
+		Category = "GLib|GameplayEffectLibrary"
+	)
+	static void AddGameplayEffectLevelByHandle(
+		UPARAM(ref) FActiveGameplayEffectHandle& Handle,
+		const int32 Delta = 1
+	);
+
+	/**
+	 * @brief Increases level by delta for all gameplay effects
+	 * @param AbilitySystem owner ability system component
+	 * @param Tags gameplay effect tags
+	 * @param Delta delta level, usually it is 1
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintAuthorityOnly,
+		Category = "GLib|GameplayEffectLibrary",
+		meta = (
+			AutoCreateRefTerm = "Tags"
+		)
+	)
+	static void AddGameplayEffectLevelByTags(
+		UPARAM(ref) UAbilitySystemComponent*& AbilitySystem,
+		const FGameplayTagContainer& Tags,
+		const int32 Delta = 1
+	);
+};

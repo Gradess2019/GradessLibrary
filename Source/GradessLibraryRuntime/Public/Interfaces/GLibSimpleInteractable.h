11e66384984d3cf3be7a36e3a10186134e41b2cb
Gradess <trofimovjava@gmail.com>
2021-10-27 01:26:03 +0300

    Add simple interactable interface

---
+++ Source/GradessLibraryRuntime/Public/Interfaces/GLibSimpleInteractable.h
@@ -0,0 +1,35 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GLibSimpleInteractable.generated.h"
+
+UINTERFACE()
+class UGLibSimpleInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * @brief A simple interface for interactable objects
+ */
+class GRADESSLIBRARYRUNTIME_API IGLibSimpleInteractable
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * @brief Interacts with current actor
+	 * @param Instigator causer of event
+	 * @return true if interacted successfully, false otherwise
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintNativeEvent,
+		Category = "GLib|SimpleInteractable"
+	)
+	bool Interact(AActor* Instigator);
+
+};

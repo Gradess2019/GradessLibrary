020503f265c9ffe71e2be5206a31c341bdd2fbe9
Gradess <trofimovjava@gmail.com>
2021-10-29 23:59:04 +0300

    Add math library

---
+++ Source/GradessLibraryRuntime/Private/Core/GLibMathLibrary.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Core/GLibMathLibrary.h"
+
+float UGLibMathLibrary::ShortestAngleBetweenVectorsInRadians(
+	const FVector& FirstVector,
+	const FVector& SecondVector
+)
+{
+	const auto BetweenQuat = FQuat::FindBetween(FirstVector, SecondVector);
+
+	auto Axis = FVector();
+	auto Angle = 0.f;
+	BetweenQuat.ToAxisAndAngle(Axis, Angle);
+
+	return Angle;
+}
+
+float UGLibMathLibrary::ShortestAngleBetweenVectorsInDegrees(const FVector& FirstVector, const FVector& SecondVector)
+{
+	const auto Radians = ShortestAngleBetweenVectorsInRadians(FirstVector, SecondVector);
+	return FMath::RadiansToDegrees(Radians);
+}

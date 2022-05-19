f2ec69b249b9cfb813780247831725a7e704e28d
Gradess <trofimovjava@gmail.com>
2022-05-19 23:06:17 +0200

    Add struct parser

--- Content/Python/glib_core/glib_wrapper_generator.py
+++ Content/Python/glib_core/glib_wrapper_generator.py
@@ -1,7 +1,44 @@
 from CppHeaderParser import *
 
 
-class GLibClassParser:
+class GLibBaseParser:
+    @classmethod
+    def parse(cls, data):
+        result = cls.parse_properties(data)
+        result += cls.parse_methods(data)
+
+        result += "};\n"
+
+        return result
+
+    @classmethod
+    def parse_properties(cls, data):
+        result = ""
+        for access_modifier, properties in data["properties"].items():
+            if len(properties) == 0:
+                continue
+
+            result += access_modifier + ":\n"
+            for class_property in properties:
+                result += GLibPropertyParser.parse(class_property) + "\n"
+
+        return result
+
+    @classmethod
+    def parse_methods(cls, data):
+        result = ""
+        for access_modifier, methods in data["methods"].items():
+            if len(methods) == 0:
+                continue
+
+            result += access_modifier + ":\n"
+            for class_method in methods:
+                result += GLibFunctionParser.parse(class_method) + "\n"
+
+        return result
+
+
+class GLibClassParser(GLibBaseParser):
     @classmethod
     def parse(cls, data):
         result = data["doxygen"] + "\n" if data.get("doxygen") else ""
@@ -11,31 +48,36 @@ class GLibClassParser:
         result += "\tGENERATED_BODY()\n"
         result += "\n"
 
-        for access_modifier, properties in data["properties"].items():
-            if len(properties) == 0:
-                continue
-
-            result += access_modifier + ":\n"
-            for class_property in properties:
-                result += GLibPropertyParser.parse(class_property) + "\n"
-
-        for access_modifier, methods in data["methods"].items():
-            if len(methods) == 0:
-                continue
-
-            result += access_modifier + ":\n"
-            for method in methods:
-                result += GLibFunctionParser.parse(method) + "\n"
-
-        result += "};\n"
+        result += super().parse(data)
 
         return result
 
 
-class GLibPropertyParser:
+class GLibStructParser(GLibBaseParser):
     @classmethod
     def parse(cls, data):
-        result = "\t" + data["doxygen"] + "\n" if data.get("doxygen") else ""
+        result = data["doxygen"] + "\n" if data.get("doxygen") else ""
+        result += "USTRUCT(BlueprintType, Blueprintable)\n"
+        result += "struct " + data["name"] + "\n"
+        result += "{\n"
+        result += "\tGENERATED_BODY()\n"
+        result += "\n"
+
+        result += super().parse(data)
+
+        return result
+
+
+class GLibMemberParser:
+    @classmethod
+    def get_doxygen(cls, data):
+        return "\t" + data["doxygen"].replace("\n", "\n\t") + "\n" if data.get("doxygen") else ""
+
+
+class GLibPropertyParser(GLibMemberParser):
+    @classmethod
+    def parse(cls, data):
+        result = cls.get_doxygen(data)
         result += "\t" + "UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = \"{category}\")" + "\n"
         category = data["category"] if data.get("category") else "GLib"
 
@@ -48,12 +90,12 @@ class GLibPropertyParser:
         return result
 
 
-class GLibFunctionParser:
+class GLibFunctionParser(GLibMemberParser):
     DECLARATION_EXTRA_SPACES = r"(?:(?<=\()\s+)|(?:\s+((?=&|;|,|\)|\())+)"
 
     @classmethod
     def parse(cls, data):
-        result = "\t" + data["doxygen"].replace("\n", "\n\t") + "\n" if data.get("doxygen") else ""
+        result = cls.get_doxygen(data)
         result += "\tUFUNCTION({callable_type}, Category = \"{category}\")\n".format_map({
             "callable_type": "BlueprintCallable" if data["rtnType"] == "void" else "BlueprintPure",
             "category": data["category"] if data.get("category") else "GLib"
@@ -74,13 +116,17 @@ class GLibWrapperGenerator:
         generated_data = ""
 
         for parsed_class, content in header.classes.items():
-            generated_data += GLibClassParser.parse(content)
+            if content["declaration_method"] == "class":
+                generated_data += GLibClassParser.parse(content)
+            elif content["declaration_method"] == "struct":
+                generated_data += GLibStructParser.parse(content)
+
             generated_data += "\n"
 
         with open("../Data/generated.h", "w+", encoding="utf-8") as f:
             f.write(generated_data)
 
-
+#
 # result = CppHeaderParser.CppHeader(r"D:\Projects\UE\5\Spacegod\Plugins\GradessLibrary\Content\Python\Data\test.h").toJSON()
 # with open(r"D:\Projects\UE\5\Spacegod\Plugins\GradessLibrary\Content\Python\Data\test.json", "w+", encoding="utf-8") as f:
 #     f.write(result)

24264226ac82f4f12c3525a9fafdc61d833e26e6
Gradess <trofimovjava@gmail.com>
2022-06-02 23:11:20 +0200

    Add IPlugin wrapper

---
+++ Source/GradessLibraryRuntime/Private/Wrappers/Core/GLibPlugin.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Wrappers/Core/GLibPlugin.h"
+#include "UObject/UObjectGlobals.h"
+
+UGLibPlugin* UGLibPlugin::CreateWrapper(TSharedPtr<IPlugin> InPlugin)
+{
+	const auto PluginWrapper = NewObject<UGLibPlugin>();
+	PluginWrapper->SetPlugin(InPlugin);
+	return PluginWrapper;
+}
+
+bool UGLibPlugin::IsValid()
+{
+	return Plugin.IsValid();
+}
+
+FString UGLibPlugin::GetName()
+{
+	return Plugin.IsValid() ? Plugin->GetName() : FString();
+}
+
+FString UGLibPlugin::GetFriendlyName()
+{
+	return Plugin.IsValid() ? Plugin->GetFriendlyName() : FString();
+}
+
+FString UGLibPlugin::GetDescriptorFileName()
+{
+	return Plugin.IsValid() ? Plugin->GetDescriptorFileName() : FString();
+}
+
+FString UGLibPlugin::GetBaseDir()
+{
+	return Plugin.IsValid() ? Plugin->GetBaseDir() : FString();
+}
+
+FString UGLibPlugin::GetContentDir()
+{
+	return Plugin.IsValid() ? Plugin->GetContentDir() : FString();
+}
+
+FString UGLibPlugin::GetMountedAssetPath()
+{
+	return Plugin.IsValid() ? Plugin->GetMountedAssetPath() : FString();
+}
+
+bool UGLibPlugin::IsEnabled()
+{
+	return Plugin.IsValid() ? Plugin->IsEnabled() : false;
+}
+
+bool UGLibPlugin::IsEnabledByDefault(bool bAllowEnginePluginsEnabledByDefault)
+{
+	return Plugin.IsValid() ? Plugin->IsEnabledByDefault(bAllowEnginePluginsEnabledByDefault) : false;
+}
+
+bool UGLibPlugin::IsHidden()
+{
+	return Plugin.IsValid() ? Plugin->IsHidden() : false;
+}
+
+bool UGLibPlugin::CanContainContent()
+{
+	return Plugin.IsValid() ? Plugin->CanContainContent() : false;
+}
+
+bool UGLibPlugin::CanContainVerse()
+{
+	return Plugin.IsValid() ? Plugin->CanContainVerse() : false;
+}

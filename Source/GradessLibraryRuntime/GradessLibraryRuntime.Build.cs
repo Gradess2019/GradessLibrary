87f9cc9e3b2fab6210fc80fefbd4e7bc87b5fb05
Gradess <trofimovjava@gmail.com>
2022-06-04 21:54:02 +0200

    Add dependency modules

--- Source/GradessLibraryRuntime/GradessLibraryRuntime.Build.cs
+++ Source/GradessLibraryRuntime/GradessLibraryRuntime.Build.cs
@@ -28,6 +28,7 @@ public class GradessLibraryRuntime : ModuleRules
 			new string[]
 			{
 				"Core",
+				"Projects"
 				// ... add other public dependencies that you statically link with here ...
 			}
 		);

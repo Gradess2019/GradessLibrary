1f781549a9d645ac756f39b34ac40227ea702ba0
Gradess <trofimovjava@gmail.com>
2022-06-05 02:06:05 +0200

    [GLIB-12] Add modules for tests

---
+++ Source/GradessLibraryTests/GradessLibraryTests.Build.cs
@@ -0,0 +1,67 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GradessLibraryTests : ModuleRules
+{
+	public GradessLibraryTests(ReadOnlyTargetRules Target) : base(Target)
+	{
+		// PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(
+			new string[]
+			{
+				// ... add public include paths required here ...
+			}
+		);
+
+
+		PrivateIncludePaths.AddRange(
+			new string[]
+			{
+				"GradessLibraryTests/Private"
+			}
+		);
+
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"FunctionalTestingEditor",
+				// ... add other public dependencies that you statically link with here ...
+			}
+		);
+
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"InputCore"
+				// ... add private dependencies that you statically link with here ...	
+			}
+		);
+
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+		);
+		
+		if (Target.bBuildDeveloperTools || Target.Configuration != UnrealTargetConfiguration.Shipping && Target.Configuration != UnrealTargetConfiguration.Test)
+		{
+			PublicDependencyModuleNames.Add("GameplayDebugger");
+			PublicDefinitions.Add("WITH_GAMEPLAY_DEBUGGER=1");
+		}
+		else
+		{
+			PublicDefinitions.Add("WITH_GAMEPLAY_DEBUGGER=0");
+		}
+	}
+}
